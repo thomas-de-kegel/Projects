fdd3f0f49baa2a8173aa5c15291d8ba8fec18533
thomas-de-kegel <thomas.dekegel@student.intecbrussel.be>
2022-10-13 10:56:53 +0200

    message functionality started

--- socketIOtest/index.js
+++ socketIOtest/index.js
@@ -2,10 +2,24 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
+const {Server} = require('socket.io');
+const io = new Server(server);
 
 app.get('/',(req, res) =>{
-    res.send('<h1>Welcome!</h1>');
+    res.sendFile(__dirname + '/index.html'); //We point to the current directory and then to the html file
 });
+
+io.on('connection', (socket)=>{
+    console.log('A user has connected!')
+    socket.on('chat message', (msg) => {
+        console.log('message: ' + msg);
+    })
+    socket.on('disconnect', ()=>{
+        console.log('A user has disconnected!')
+    });
+});
+
+
 
 server.listen(3000, ()=>{
     console.log('listening on port 3000');
